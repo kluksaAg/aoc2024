f400f7cdc0b43924d29c9f77656818881f9bbcad
Luksa Kraljevic <luksa.kraljevic@wearenotch.com>
2024-12-05 16:22:57 +0100

    Fix tests

--- src/bin/03.rs
+++ src/bin/03.rs
@@ -41,13 +41,13 @@ mod tests {
 
     #[test]
     fn test_part_one() {
-        let result = part_one(&advent_of_code::template::read_file("inputs", DAY));
-        // assert_eq!(result, Some(161));
+        let result = part_one(&advent_of_code::template::read_file("examples", DAY));
+        assert_eq!(result, Some(161));
     }
 
     #[test]
     fn test_part_two() {
-        let result = part_two(&advent_of_code::template::read_file("inputs", DAY));
-        // assert_eq!(result, Some(161));
+        let result = part_two(&advent_of_code::template::read_file("examples", DAY));
+        assert_eq!(result, Some(161));
     }
 }
